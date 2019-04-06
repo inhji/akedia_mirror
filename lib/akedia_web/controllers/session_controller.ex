1c47fa36425ceaaf7dedd04891d43fb96a214364
Inhji <johnnie@posteo.de>
2019-04-07 00:01:43 +0200

    redirect to referer on login

--- lib/akedia_web/controllers/session_controller.ex
+++ lib/akedia_web/controllers/session_controller.ex
@@ -4,16 +4,31 @@ defmodule AkediaWeb.SessionController do
 
   def new(conn, _params) do
     changeset = Accounts.change_credential(%Accounts.Credential{})
-    render(conn, "new.html", changeset: changeset)
+    referer = case get_req_header(conn, "referer") do
+      [referer] -> referer
+      _ -> nil
+    end
+
+    conn
+    |> Plug.Conn.put_session(:referer, referer)
+    |> render("new.html", changeset: changeset)
   end
 
   def create(conn, %{"credential" => %{"email" => email, "password" => password}}) do
     case Auth.authenticate_user(email, password) do
       {:ok, user} ->
+
+        redirect_path =
+          case conn |> get_session(:referer) do
+            nil -> Routes.user_path(conn, :show)
+            referer -> URI.parse(referer).path
+          end
+
         conn
         |> Auth.login(user)
         |> put_flash(:info, "Welcome back, #{user.name}")
-        |> redirect(to: Routes.user_path(conn, :show))
+        |> delete_session(:referer)
+        |> redirect(to: redirect_path)
       {:error, reason} ->
         changeset = Accounts.change_credential(%Accounts.Credential{email: email})
 
