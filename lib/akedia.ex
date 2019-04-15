211a5d70d394dfbf58815b68daa3363c0931c284
Inhji <johnnie@posteo.de>
2019-04-15 22:15:00 +0200

    add global url helper

--- lib/akedia.ex
+++ lib/akedia.ex
@@ -6,4 +6,11 @@ defmodule Akedia do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  alias AkediaWeb.{Router, Endpoint}
+
+  def url do
+    Router.Helpers.url(Endpoint)
+    |> URI.parse()
+    |> to_string()
+  end
 end
