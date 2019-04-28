ebde05c0123e165ff8f5900b3b2eff19cbd0602e
Inhji <johnnie@posteo.de>
2019-04-28 11:11:30 +0200

    add indieweb package

--- mix.exs
+++ mix.exs
@@ -55,7 +55,8 @@ defmodule Akedia.MixProject do
       {:edeliver, "~> 1.6"},
       {:plug_micropub, git: "https://github.com/inhji/plug_micropub.git", branch: "master"},
       {:httpoison, "~> 1.5"},
-      {:timex, "~> 3.5"}
+      {:timex, "~> 3.5"},
+      {:indieweb, "~> 0.0.42"}
     ]
   end
 
