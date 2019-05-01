0314e74a1f7cc4238aa821fe61de305e476516a2
Inhji <johnnie@posteo.de>
2019-05-01 22:18:00 +0200

    add seed command for listenbrainz import

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,21 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+seed_file = "./data.json"
+
+if File.exists?(seed_file) do
+  contents = File.read!(seed_file)
+  list = Jason.decode!(contents)
+
+  list
+  |> Enum.map(fn item ->
+    %{
+      :artist => item["artist_name"],
+      :album => item["release_name"],
+      :track => item["track_name"],
+      :listened_at => DateTime.from_unix!(item["timestamp"])
+    }
+  end)
+  |> Enum.each(&Akedia.Tracks.create_track/1)
+end
