fcc4cf7fb639f5a78e4ee8d7192ce53100a84ff6
Inhji <johnnie@posteo.de>
2019-05-08 12:05:21 +0200

    add grayscale filter to unpublished stories

--- lib/akedia_web/views/story_view.ex
+++ lib/akedia_web/views/story_view.ex
@@ -14,16 +14,25 @@ defmodule AkediaWeb.StoryView do
   end
 
   def intro_style(story) do
-    case Enum.empty?(story.entity.images) do
+    image_style = case Enum.empty?(story.entity.images) do
       false -> "background-image: url('#{image_url(List.first(story.entity.images), :original)}')"
       true -> ""
     end
+
+    published_style = case story.entity.is_published do
+      false -> "-webkit-filter: grayscale(100%); grayscale(100%);"
+      true -> ""
+    end
+
+    Enum.join([image_style, published_style], "; ")
   end
 
   def intro_class(story) do
-    case Enum.empty?(story.entity.images) do
+    gradient_class = case Enum.empty?(story.entity.images) do
       true -> "bg-gradient-primary"
       false -> ""
     end
+
+    Enum.join([gradient_class], " ")
   end
 end
