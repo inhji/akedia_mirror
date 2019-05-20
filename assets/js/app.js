c514c3ccbb7c39c74c70465e559e2a048ceaa760
Inhji <johnnie@posteo.de>
2019-05-20 09:09:49 +0200

    cleanup

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,10 +31,10 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-      .then(function(reg){
-        console.log("Service worker registered.");
-     }).catch(function(err) {
-        console.log("Service worker not registered. This happened:", err)
-    });
+  navigator.serviceWorker.register('/sw.js')
+    .then(function(reg){
+      console.log("Service worker registered.");
+   }).catch(function(err) {
+      console.log("Service worker not registered. This happened:", err)
+  });
  }
