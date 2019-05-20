d09581864e9648d366c5943ffea9ee1b81dcf3a6
Inhji <johnnie@posteo.de>
2019-05-20 09:14:27 +0200

    work on webmention sending

--- lib/akedia/indie/webmentions.ex
+++ lib/akedia/indie/webmentions.ex
@@ -1,5 +1,6 @@
 defmodule Akedia.Indie.Webmentions do
   require Logger
+  alias Akedia.HTTP
 
   def do_send_webmentions(url, selector) do
     case Webmentions.send_webmentions(url, selector) do
@@ -15,5 +16,46 @@ defmodule Akedia.Indie.Webmentions do
       {:error, error} ->
         Logger.error(error)
     end
+  end
+
+  def send_webmentions(%{:entity_id => _entity_id} = schema) do
+    url = Akedia.url(schema)
+
+    # TODO: Finish this implementation?
+    # Goal here is to see the full response in case of an error
+    # To examine what brid.gy does, for example when trying to update
+    # a github star/comment
+  end
+
+  def fetch_links(url, selector \\ "") do
+    selector = String.trim(selector <> " a[href]")
+    base_url = HTTP.base_url(url)
+
+    html =
+      case HTTP.get(url) do
+        {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
+          body
+
+        {:ok, %HTTPoison.Response{status_code: code}} ->
+          {:error, "Could not fetch url, got code #{code}"}
+
+        {:error, error} ->
+          {:error, error}
+      end
+
+    links =
+      html
+      |> Floki.parse()
+      |> Floki.find(selector)
+      |> Enum.map(&extract_href/1)
+      |> Enum.filter(fn url -> url != "#" end)
+      |> Enum.map(&Akedia.HTTP.abs_url(base_url, &1))
+
+    {:ok, links}
+  end
+
+  def extract_href(node) do
+    Floki.attribute(node, "a", "href")
+    |> List.first()
   end
 end
