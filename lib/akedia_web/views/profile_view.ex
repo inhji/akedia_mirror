ac8703e01c53ed89645c50b755460238c4ddde00
Inhji <johnnie@posteo.de>
2019-05-26 10:56:25 +0200

    profile and user templates -> bulma

--- lib/akedia_web/views/profile_view.ex
+++ lib/akedia_web/views/profile_view.ex
@@ -1,3 +1,10 @@
 defmodule AkediaWeb.ProfileView do
   use AkediaWeb, :view
+
+  def visibility(profile) do
+    case profile.public do
+      true -> "public"
+      _ -> "private"
+    end
+  end
 end
