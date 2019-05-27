2a715cc79945454394fc06a86c06b15e3a06b429
Inhji <johnnie@posteo.de>
2019-05-27 19:05:32 +0200

    improve favicon fetching

--- lib/akedia/indie/microformats/hcard.ex
+++ lib/akedia/indie/microformats/hcard.ex
@@ -2,9 +2,12 @@ defmodule Akedia.Indie.Microformats.HCard do
   defstruct name: "", photo: ""
 
   def parse(%{:name => [name], :photo => [photo]} = properties) when is_map(properties) do
-    %Akedia.Indie.Microformats.HCard{
-      name: name,
-      photo: photo
-    }
+    {:ok,
+     %Akedia.Indie.Microformats.HCard{
+       name: name,
+       photo: photo
+     }}
   end
+
+  def parse(_), do: {:error, :no_hcard_found}
 end
--- lib/akedia/workers/favicon.ex
+++ lib/akedia/workers/favicon.ex
@@ -4,10 +4,16 @@ defmodule Akedia.Workers.Favicon do
   alias Akedia.{Repo, Media, HTTP}
   alias Akedia.Content.Bookmark
   alias Akedia.Indie.{Microformats, Favicon}
+  alias Akedia.Indie.Microformats.HCard
 
   def perform(%Bookmark{url: bookmark_url} = bookmark) do
     case get_favicon(bookmark_url) do
-      {:ok, favicon_url} ->
+      nil ->
+        Logger.debug("No favicon found for #{bookmark_url}")
+
+      favicon_url ->
+        Logger.debug(favicon_url)
+
         favicon =
           bookmark_url
           |> HTTP.hostname()
@@ -16,9 +22,6 @@ defmodule Akedia.Workers.Favicon do
         bookmark
         |> Bookmark.changeset(%{favicon_id: favicon.id})
         |> Repo.update!()
-
-      {:error, :no_favicon_found} ->
-        Logger.debug("No favicon found for #{bookmark_url}")
     end
   end
 
@@ -33,20 +36,38 @@ defmodule Akedia.Workers.Favicon do
     end
   end
 
-  def get_favicon(url) do
-    case Microformats.fetch_hcard(url) do
-      {:ok, properties} ->
-        hcard = Microformats.HCard.parse(properties)
-        {:ok, hcard.photo}
+  def maybe_get_favicon_from_hcard(url) do
+    with {:ok, properties} <- Microformats.fetch_hcard(url),
+         {:ok, %HCard{photo: photo}} <- HCard.parse(properties) do
+      {:ok, photo}
+    else
+      {:error, _} -> {:error, :no_favicon_in_hcard}
+    end
+  end
+
+  def maybe_get_favicon_from_head(url) do
+    case Favicon.fetch(url) do
+      {:ok, favicon} ->
+        {:ok, favicon}
 
       {:error, _error} ->
-        case Favicon.fetch(url) do
-          {:ok, favicon} ->
-            {:ok, favicon}
-
-          {:error, _error} ->
-            {:error, :no_favicon_found}
-        end
+        {:error, :no_favicon_in_head}
     end
+  end
+
+  def get_favicon(url) do
+    Enum.reduce_while(
+      [
+        &maybe_get_favicon_from_hcard/1,
+        &maybe_get_favicon_from_head/1
+      ],
+      nil,
+      fn strategy, acc ->
+        case strategy.(url) do
+          {:error, _} -> {:cont, acc}
+          {:ok, favicon} -> {:halt, favicon}
+        end
+      end
+    )
   end
 end
