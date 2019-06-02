298a1e8204528f180a6eb4220b346fd8b8c7b5ec
Inhji <johnnie@posteo.de>
2019-06-02 10:33:59 +0200

    add link to musicbrainz to make finding mbid easier

--- lib/akedia_web/views/album_view.ex
+++ lib/akedia_web/views/album_view.ex
@@ -1,3 +1,10 @@
 defmodule AkediaWeb.AlbumView do
   use AkediaWeb, :view
+
+  def musicbrainz_query_url(album) do
+    artist = album.artist.name
+    |> URI.encode_www_form()
+    |> String.downcase()
+    "https://musicbrainz.org/search?query=#{artist}&type=artist&method=indexed"
+  end
 end
