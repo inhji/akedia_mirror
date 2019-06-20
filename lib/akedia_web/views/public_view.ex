0ba20c18a76deee1742309be459761296a793be1
Inhji <johnnie@posteo.de>
2019-06-21 01:48:21 +0200

    consolidate post rendering

--- lib/akedia_web/views/public_view.ex
+++ lib/akedia_web/views/public_view.ex
@@ -1,6 +1,7 @@
 defmodule AkediaWeb.PublicView do
   use AkediaWeb, :view
   import Scrivener.HTML
+  alias AkediaWeb.{BookmarkView, PostView, LikeView}
 
   def render("meta.index.html", assigns) do
     [title("Home", assigns)]
