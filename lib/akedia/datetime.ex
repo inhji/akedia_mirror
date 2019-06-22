77f1511fb4c6008e2b29fd9c72a83f35c2ae8e93
Inhji <johnnie@posteo.de>
2019-06-22 19:33:39 +0200

    &maybe_convert_from_strange_german_format/1

--- lib/akedia/datetime.ex
+++ lib/akedia/datetime.ex
@@ -3,7 +3,8 @@ defmodule Akedia.DateTime do
     Enum.reduce_while(
       [
         &maybe_convert_from_iso_8601/1,
-        &maybe_convert_from_rfc_1123/1
+        &maybe_convert_from_rfc_1123/1,
+        &maybe_convert_from_strange_german_format/1
       ],
       nil,
       fn strategy, acc ->
@@ -18,9 +19,10 @@ defmodule Akedia.DateTime do
   def to_datetime_utc(nil), do: nil
 
   def to_datetime_utc(datetime_string) when is_binary(datetime_string) do
-    {:ok, datetime_utc} = datetime_string
-    |> to_datetime()
-    |> DateTime.shift_zone("Etc/UTC")
+    {:ok, datetime_utc} =
+      datetime_string
+      |> to_datetime()
+      |> DateTime.shift_zone("Etc/UTC")
 
     datetime_utc
   end
@@ -30,10 +32,14 @@ defmodule Akedia.DateTime do
   end
 
   defp maybe_convert_from_iso_8601(datetime_string) do
-    Timex.parse(datetime_string,"{ISO:Extended:Z}")
+    Timex.parse(datetime_string, "{ISO:Extended:Z}")
   end
 
   defp maybe_convert_from_rfc_1123(datetime_string) do
     Timex.parse(datetime_string, "{RFC1123}")
+  end
+
+  defp maybe_convert_from_strange_german_format(datetime_string) do
+    Timex.parse(datetime_string, "%d %b %Y %T %z", Timex.Parse.DateTime.Tokenizers.Strftime)
   end
 end
