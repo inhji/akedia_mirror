b6c90ba1f3cc99697f746a120a0d53d3a87aa294
Inhji <johnnie@posteo.de>
2019-06-28 08:46:50 +0200

    work on stories

--- lib/akedia_web/views/story_view.ex
+++ lib/akedia_web/views/story_view.ex
@@ -11,27 +11,4 @@ defmodule AkediaWeb.StoryView do
 
   def render("scripts.edit.html", assigns), do: AkediaWeb.Helpers.Meta.admin_scripts(assigns)
   def render("scripts.new.html", assigns), do: AkediaWeb.Helpers.Meta.admin_scripts(assigns)
-
-  def intro_style(story) do
-    image_style = case Enum.empty?(story.entity.images) do
-      false -> "background-image: url('#{image_url(List.first(story.entity.images), :original)}')"
-      true -> ""
-    end
-
-    published_style = case story.entity.is_published do
-      false -> "-webkit-filter: grayscale(100%); grayscale(100%);"
-      true -> ""
-    end
-
-    Enum.join([image_style, published_style], "; ")
-  end
-
-  def intro_class(story) do
-    gradient_class = case Enum.empty?(story.entity.images) do
-      true -> "bg-gradient-primary"
-      false -> ""
-    end
-
-    Enum.join([gradient_class], " ")
-  end
 end
