aefec65a76fe4f3400327bf1c0c92af437a99bc8
Inhji <johnnie@posteo.de>
2019-06-30 14:59:32 +0200

    allow deleting of feeds along with their entries

---
+++ priv/repo/migrations/20190630125520_delete_entries_on_feed_delete.exs
@@ -0,0 +1,17 @@
+defmodule Akedia.Repo.Migrations.DeleteEntriesOnFeedDelete do
+  use Ecto.Migration
+
+  def up do
+    execute("ALTER TABLE feed_entries DROP CONSTRAINT feed_entries_feed_id_fkey")
+    alter table(:feed_entries) do
+      modify :feed_id, references(:feeds, on_delete: :delete_all)
+    end
+  end
+
+  def down do
+    execute("ALTER TABLE feed_entries DROP CONSTRAINT feed_entries_feed_id_fkey")
+    alter table(:feed_entries) do
+      modify :feed_id, references(:feeds, on_delete: :nothing)
+    end
+  end
+end
