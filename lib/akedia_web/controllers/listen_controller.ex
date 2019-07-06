a7f5d4dd7ab672b74714ae1377c45c2d0eefef47
Inhji <johnnie@posteo.de>
2019-07-06 11:11:07 +0200

    spice up listen sidebar

--- lib/akedia_web/controllers/listen_controller.ex
+++ lib/akedia_web/controllers/listen_controller.ex
@@ -5,7 +5,8 @@ defmodule AkediaWeb.ListenController do
 
   def index(conn, params) do
     page = Listens.listens_paginated(params)
-    albums = Listens.group_by_album(9, weeks: -1)
+    albums_weekly = Listens.group_by_album(5, weeks: -1)
+    albums_overall = Listens.group_by_album(5)
 
     render(conn, "index.html",
       page: page,
@@ -14,7 +15,17 @@ defmodule AkediaWeb.ListenController do
       page_size: page.page_size,
       total_pages: page.total_pages,
       total_entries: page.total_entries,
-      albums: albums
+      albums_weekly: albums_weekly,
+      max_weekly: get_max(albums_weekly),
+      albums_overall: albums_overall,
+      max_overall: get_max(albums_overall)
     )
+  end
+
+  defp get_max(albums) do
+    case List.first(albums) do
+      nil -> 0
+      albums -> Map.get(albums, :listens)
+    end
   end
 end
