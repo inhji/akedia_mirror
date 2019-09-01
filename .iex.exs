229b020423d2451dd097598795ba95b9509f257d
Inhji <johnnie@posteo.de>
2019-09-01 10:17:47 +0200

    update iex config

--- .iex.exs
+++ .iex.exs
@@ -1,7 +1,7 @@
 import Ecto.Query
 
 alias Akedia.Content
-alias Akedia.Content.{Post, Bookmark, Page, Story, Like, Entity}
+alias Akedia.Content.{Post, Bookmark, Story, Like, Entity}
 alias Akedia.Listens.{Listen, Album, Artist}
 alias Akedia.Media.Image
 alias Akedia.Indie.Microsub
