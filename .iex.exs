4b0c06d499c9d19ec5bc702de83c0ccd7b432676
Inhji <johnnie@posteo.de>
2019-09-15 11:26:44 +0200

    update .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -2,6 +2,7 @@ import Ecto.Query
 
 alias Akedia.Content
 alias Akedia.Content.{Post, Bookmark, Story, Like, Entity}
+alias Akedia.Listens
 alias Akedia.Listens.{Listen, Album, Artist}
 alias Akedia.Media.Image
 alias Akedia.Indie.Microsub
