f95092c0a9f41fcde2e8ab651afbed87b3124582
Inhji <johnnie@posteo.de>
2020-02-09 00:33:15 +0100

    add rsa keypair

--- lib/akedia/accounts/user.ex
+++ lib/akedia/accounts/user.ex
@@ -15,6 +15,9 @@ defmodule Akedia.Accounts.User do
     field :avatar, Akedia.Media.AvatarUploader.Type
     field :cover, Akedia.Media.CoverUploader.Type
 
+    field :priv_key, :string
+    field :pub_key, :string
+
     has_one(:credential, Credential)
     has_many(:profiles, Profile)
 
@@ -26,7 +29,23 @@ defmodule Akedia.Accounts.User do
     user
     |> cast(attrs, [:name, :username, :now, :about, :tagline])
     |> cast_attachments(attrs, [:avatar, :cover])
-    |> validate_required([:name, :username])
+    |> maybe_generate_pub_key_pair()
+    |> validate_required([:name, :username, :priv_key, :pub_key])
     |> unique_constraint(:username)
+  end
+
+  @doc false
+  defp maybe_generate_pub_key_pair(changeset) do
+    case get_field(changeset, :priv_key) do
+      nil ->
+        {:ok, {priv, pub}} = RsaEx.generate_keypair()
+
+        changeset
+        |> put_change(:priv_key, priv)
+        |> put_change(:pub_key, pub)
+
+      _ ->
+        changeset
+    end
   end
 end
