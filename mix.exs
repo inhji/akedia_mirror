6fcc30633a38965e8d3df64a4b72760bdf236936
Inhji <johnnie@posteo.de>
2020-02-10 07:30:07 +0100

    version 0.78.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Akedia.MixProject do
   def project do
     [
       app: :akedia,
-      version: "0.77.5",
+      version: "0.78.0",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
