b8b39b3d7efb1cdfa5bada7f4b9ba34c50e8c46c
Inhji <johnnie@posteo.de>
2020-02-15 10:20:26 +0100

    clean up unused modules

--- lib/akedia_web/endpoint.ex
+++ lib/akedia_web/endpoint.ex
@@ -1,10 +1,6 @@
 defmodule AkediaWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :akedia
 
-  socket "/socket", AkediaWeb.AdminSocket,
-    websocket: true,
-    longpoll: false
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
