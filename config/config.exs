3603e16db301dc09bf9d3d6c7df12030d4f2ca93
Inhji <johnnie@posteo.de>
2020-11-02 18:33:00 +0100

    feat: add git_ops

--- config/config.exs
+++ config/config.exs
@@ -32,6 +32,30 @@ config :akedia, Akedia.Config,
     "channels"
   ]
 
+config :git_ops,
+  mix_project: Tomie.Umbrella.MixProject,
+  changelog_file: "CHANGELOG.md",
+  repository_url: "https://git.inhji.de/inhji/akedia",
+  types: [
+    # Makes an allowed commit type called `tidbit` that is not
+    # shown in the changelog
+    tidbit: [
+      hidden?: true
+    ],
+    # Makes an allowed commit type called `important` that gets
+    # a section in the changelog with the header "Important Changes"
+    important: [header: "Important Changes"],
+    tweak: [header: "Tweaks"],
+    ref: [header: "Refactors"]
+  ],
+  # Instructs the tool to manage your mix version in your `mix.exs` file
+  # See below for more information
+  manage_mix_version?: true,
+  # Instructs the tool to manage the version in your README.md
+  # Pass in `true` to use `"README.md"` or a string to customize
+  manage_readme_version: "README.md",
+  version_tag_prefix: "v"
+
 # Configures the endpoint
 config :akedia, AkediaWeb.Endpoint,
   url: [host: "localhost"],
