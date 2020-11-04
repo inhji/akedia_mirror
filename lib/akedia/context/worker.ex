c77acd917ad8790ec2e67f1c30173194860bd16b
Inhji <johnnie@posteo.de>
2020-11-04 09:02:46 +0100

    fix: catch errors in context worker

--- lib/akedia/context/worker.ex
+++ lib/akedia/context/worker.ex
@@ -1,35 +1,38 @@
 defmodule Akedia.Context.Worker do
-  require Logger
-  use Que.Worker
-  alias Akedia.Content.Like
-  alias Akedia.Context
+	require Logger
+	use Que.Worker
+	alias Akedia.Content.Like
+	alias Akedia.Context
 
-  def perform(%Like{url: url, entity: entity} = _like) do
-    with author_map <- Context.get_author(url),
-         content_map <- Context.get_content(url) do
-      Logger.debug("Creating author..")
+	def perform(%Like{url: url, entity: entity} = _like) do
+		with author_map <- Context.get_author(url),
+				 content_map <- Context.get_content(url) do
+			Logger.debug("Creating author..")
 
-      {:ok, author} =
-        author_map
-        |> Map.drop([:photo])
-        |> Akedia.Indie.maybe_create_author()
+			{:ok, author} =
+				author_map
+				|> Map.drop([:photo])
+				|> Akedia.Indie.maybe_create_author()
 
-      Logger.debug("Saving author photo..")
+			Logger.debug("Saving author photo..")
 
-      {:ok, author} = Akedia.Indie.update_author(author, %{photo: author_map[:photo]})
+			{:ok, author} = Akedia.Indie.update_author(author, %{photo: author_map[:photo]})
 
-      Logger.debug("Creating context..")
+			Logger.debug("Creating context..")
 
-      {:ok, context} =
-        content_map
-        |> Map.drop([:photo])
-        |> Map.put_new(:author_id, author.id)
-        |> Map.put_new(:entity_id, entity.id)
-        |> Akedia.Indie.maybe_create_context()
+			{:ok, context} =
+				content_map
+				|> Map.drop([:photo])
+				|> Map.put_new(:author_id, author.id)
+				|> Map.put_new(:entity_id, entity.id)
+				|> Akedia.Indie.maybe_create_context()
 
-      Logger.debug("Saving context photo..")
+			Logger.debug("Saving context photo..")
 
-      {:ok, _context} = Akedia.Indie.update_context(context, %{photo: content_map[:photo]})
-    end
-  end
+			{:ok, _context} = Akedia.Indie.update_context(context, %{photo: content_map[:photo]})
+		else
+			error ->
+				Logger.error(inspect(error))
+		end
+	end
 end
