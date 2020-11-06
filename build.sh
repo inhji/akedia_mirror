b497903cb8b64851d351b793c5719a2d714b1684
Inhji <johnnie@posteo.de>
2020-11-06 08:46:56 +0100

    chore: even prettier build script

--- build.sh
+++ build.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 
+echo ""
 echo "Installing Hex/Rebar"
 echo "--------------------------"
 MIX_ENV=prod mix local.hex --if-missing --force
 MIX_ENV=prod mix local.rebar --if-missing --force
 
+echo ""
 echo "Getting/compiling Hex dependencies"
 echo "--------------------------"
 MIX_ENV=prod mix deps.get --only prod
 MIX_ENV=prod mix deps.compile
 
+echo ""
 echo "Getting/compiling NPM dependencies"
 echo "--------------------------"
 npm install --prefix ./assets
 NODE_ENV=production npm run deploy --prefix ./assets
 MIX_ENV=prod mix phx.digest
 
+echo ""
 echo "Generating release"
 echo "--------------------------"
 MIX_ENV=prod mix release --overwrite
