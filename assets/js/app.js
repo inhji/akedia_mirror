a851cc75fb3ae03ac6ec2076147012700c4a758b
Inhji <johnnie@posteo.de>
2020-11-07 11:45:13 +0100

    feat: optimize webpack build

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,3 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
-import "../css/app.scss"
-
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
 // in "webpack.config.js".
@@ -18,7 +13,7 @@ import "phoenix_html"
 import "./webauthn/login.js"
 import "./webauthn/register.js"
 
-import Skycons from '../vendor/skycons.js'
+import Skycons from './skycons.js'
 
 // Register Service Worker
 if ('serviceWorker' in navigator) {
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,76 +1,67 @@
 const path = require('path');
+const glob = require('glob');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-module.exports = (env, options) => ({
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin(),
-      new CssMinimizerPlugin()
-    ]
-  },
-  entry: {
-    app: './js/app.js'
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, '../priv/static/js')
-  },
-  module: {
-    rules: [{
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.(sass|scss)$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: ''
-            }
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 2,
-              sourceMap: true
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: '../fonts'
-          }
-        }]
-      }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '../css/[name].css'
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'static/', to: '../' }
+module.exports = (env, options) => {
+  const devMode = options.mode !== 'production';
+
+  return {
+    optimization: {
+      minimizer: [
+        new TerserPlugin({ parallel: true }),
+        new OptimizeCSSAssetsPlugin({})
       ]
-    })
-  ]
-});
+    },
+    entry: {
+      'app': './js/app.js',
+      'styles': './js/styles.js'
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, '../priv/static/js'),
+      publicPath: '/js/'
+    },
+    devtool: devMode ? 'eval-cheap-module-source-map' : undefined,
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true
+            }
+          }
+        },
+        {
+          test: /\.[s]?css$/,
+          use: [
+            devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(ttf|eot|woff|woff2|svg)$/,
+          use: [{
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: '../fonts'
+            }
+          }]
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({ filename: '../css/app.css' }),
+      new CopyWebpackPlugin({
+        patterns: [{ from: 'static/', to: '../' }]
+      })
+    ]
+  }
+}
