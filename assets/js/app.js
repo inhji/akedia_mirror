eb6b2ef35fc4aad4d7a68157d8ec53c2e550c35b
Inhji <johnnie@posteo.de>
2020-11-08 10:06:17 +0100

    feat: add nprogress

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,10 +6,16 @@ import "../css/app.scss"
 import "phoenix_html"
 import loadView from './views/loader';
 import {Socket} from "phoenix"
+import NProgress from "nprogress"
 import LiveSocket from "phoenix_live_view"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+
+
+// Show progress bar on live navigation and form submits
+window.addEventListener("phx:page-loading-start", info => NProgress.start())
+window.addEventListener("phx:page-loading-stop", info => NProgress.done())
 
 // Connect if there are any LiveViews on the page
 liveSocket.connect()
