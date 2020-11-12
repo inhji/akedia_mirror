c43fc4ca092329eaf34e7b9565f869abbd9eda37
Inhji <johnnie@posteo.de>
2020-11-12 08:32:46 +0100

    fix: view loader string

--- lib/akedia_web/views/layout_view.ex
+++ lib/akedia_web/views/layout_view.ex
@@ -10,25 +10,24 @@ defmodule AkediaWeb.LayoutView do
   in this combination of view/template.
   """
   def js_view_path(conn) do
-    [view_name(conn), template_name(conn)]
+    [controller_name(conn), template_name(conn)]
     |> Enum.join("/")
   end
 
   # Takes the resource name of the view module and removes the
   # the ending *_view* string.
-  defp view_name(%{private: private} = _conn) do
+  defp controller_name(%{private: private} = _conn) do
     private
-    |> Map.get(:phoenix_view)
+    |> Map.get(:phoenix_controller)
     |> Phoenix.Naming.resource_name()
-    |> String.replace("_view", "")
+    |> String.replace("_controller", "")
   end
 
   # Removes the extion from the template and reutrns
   # just the name.
   defp template_name(%{private: private} = _conn) do
     private
-    |> Map.get(:phoenix_template)
-    |> String.split(".")
-    |> Enum.at(0)
+    |> Map.get(:phoenix_action)
+    |> to_string()
   end
 end
