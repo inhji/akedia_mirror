3e9d13b22614a03e32e59cd9417f6a3238d1d137
Inhji <johnnie@posteo.de>
2020-11-14 10:43:07 +0100

    fix: improve post form live, first working version

--- lib/akedia_web/live/post_form_live.ex
+++ lib/akedia_web/live/post_form_live.ex
@@ -1,24 +1,46 @@
 defmodule AkediaWeb.PostFormLive do
   use AkediaWeb, :live
-  alias Akedia.Content.Post
+  alias Akedia.Content.{Post, Bookmark, Like}
 
   def mount(_params, _assigns, socket) do
     changeset = Post.changeset(%Post{}, %{})
 
-    {:ok, assign(socket, changeset: changeset, tags: [])}
+    {:ok, assign(socket, changeset: changeset, type: "post", params: %{})}
+  end
+
+  def handle_event("change_type", %{"type" => type}, %{assigns: assigns} = socket) do
+    changeset =
+      if type !== assigns.type do
+        get_changeset_by_type(assigns.params, type)
+      else
+        assigns.changeset
+      end
+
+    {:noreply, assign(socket, type: type, changeset: changeset, params: assigns.params)}
   end
 
   def handle_event("validate", %{"post" => params}, socket) do
     changeset =
-      %Post{}
-      |> Post.changeset(params)
+      params
+      |> get_changeset_by_type(socket.assigns.type)
       |> Map.put(:action, :insert)
 
-    {:noreply, assign(socket, changeset: changeset, tags: [])}
+    {:noreply, assign(socket, changeset: changeset, params: params)}
   end
 
-  def handle_event("save", %{"post" => post_params}, socket) do
-    case Akedia.Content.create_post(post_params) do
+  def handle_event("save", %{"post" => params}, socket) do
+    IO.inspect(params)
+
+    result =
+      case socket.assigns.type do
+        "post" -> Akedia.Content.create_post(params)
+        "like" -> Akedia.Content.create_like(params)
+        "bookmark" -> Akedia.Content.create_bookmark(params)
+      end
+
+    IO.inspect(result)
+
+    case result do
       {:ok, post} ->
         %{entity_id: post.entity_id}
         |> Akedia.Webmentions.Worker.new()
@@ -30,21 +52,15 @@ defmodule AkediaWeb.PostFormLive do
          |> redirect(to: Routes.public_path(AkediaWeb.Endpoint, :index))}
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, changeset: changeset, tags: [])}
+        {:noreply, assign(socket, changeset: changeset)}
     end
   end
 
-  def handle_event("more", _params, socket) do
-    changeset = socket.assigns.changeset
-
-    query_params =
-      case changeset.valid? do
-        true -> %{:content => Ecto.Changeset.get_change(changeset, :content)}
-        _ -> %{}
-      end
-
-    {:noreply,
-     socket
-     |> redirect(to: Routes.post_path(AkediaWeb.Endpoint, :new, query_params))}
+  def get_changeset_by_type(changes, type) do
+    case type do
+      "post" -> Post.changeset(%Post{}, changes)
+      "bookmark" -> Bookmark.changeset(%Bookmark{}, changes)
+      "like" -> Like.changeset(%Like{}, changes)
+    end
   end
 end
